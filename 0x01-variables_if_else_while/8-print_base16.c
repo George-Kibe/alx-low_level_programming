bd5a46a0124b123433ce701ca122b7ecdf314552
George-Kibe <georgekibewambui@gmail.com>
2023-02-17 19:56:12 +0300

    0x01-variables_if_else_while numbers 4 to 10

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+    int i;
+    for (i = 0; i < 16; i++) {
+        if (i < 10) {
+            putchar(i + 48);
+        } else {
+            putchar(i + 87);
+        }
+    }
+    putchar('\n');
+    return 0;
+}
