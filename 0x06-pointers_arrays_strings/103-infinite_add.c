82efc2d725f1f75169899e306588e202ce67910d
George-Kibe <georgekibewambui@gmail.com>
2023-03-02 23:26:31 +0300

    pointers arrays and strings corrections

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,46 +1,53 @@
-#include <stdio.h>
-
-/*
- *Find the lengths of the input numbers
- *Check if the result can fit in the output buffer
- *Start adding from the rightmost digits
- *Reverse the result string in-place
- *Null-terminate the result string 
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store the result
+ * @size_r: buffer size
+ *
+ * Return: pointer to the result, or 0 if the result cannot be stored in r
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, carry = 0, sum = 0;
+	int len1 = 0, len2 = 0, carry = 0, sum = 0, i = 0, j = 0;
+	char *res;
 
-    while (n1[len1] != '\0') {
-        len1++;
-    }
-    while (n2[len2] != '\0') {
-        len2++;
-    }
+	while (n1[len1])
+		len1++;
+	while (n2[len2])
+		len2++;
 
-    if (len1 + len2 + 1 > size_r) {
-        return 0;
-    }
+	if (len1 + len2 >= size_r)
+		return (0);
 
-    int i = len1 - 1, j = len2 - 1, k = 0;
-    while (i >= 0 || j >= 0 || carry > 0) {
-        int d1 = i >= 0 ? n1[i] - '0' : 0;
-        int d2 = j >= 0 ? n2[j] - '0' : 0;
-        sum = d1 + d2 + carry;
-        carry = sum / 10;
-        r[k++] = sum % 10 + '0';
-        i--;
-        j--;
-    }
+	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
 
-    for (i = 0, j = k - 1; i < j; i++, j--) {
-        char temp = r[i];
-        r[i] = r[j];
-        r[j] = temp;
-    }
+		if (sum >= 10)
+		{
+			carry = 1;
+			sum -= 10;
+		}
+		else
+			carry = 0;
 
-    r[k] = '\0';
+		r[i + j + 1] = sum + '0';
+	}
 
-    return r;
+	if (carry)
+	{
+		if (len1 + len2 + 1 >= size_r)
+			return (0);
+		r[0] = '1';
+		res = r;
+	}
+	else
+		res = r + 1;
+
+	return (res);
 }
