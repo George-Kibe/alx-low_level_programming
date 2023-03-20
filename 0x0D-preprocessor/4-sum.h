6d042401dbaa5c3172972aeb4e26fcb918bf397d
George-Kibe <georgekibewambui@gmail.com>
2023-03-20 16:51:37 +0300

    preprocessor

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+/**
+ * SUM - Computes the sum of two numbers.
+ * @x: The first number to add.
+ * @y: The second number to add.
+ *
+ * Return: The sum of x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_MACRO_H */
