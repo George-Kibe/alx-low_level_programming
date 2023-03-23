9d542f26e40ca2c9f03210e2a1d09f6df673b880
George-Kibe <georgekibewambui@gmail.com>
2023-03-24 00:24:17 +0300

    variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* MY_HEADER_H */
