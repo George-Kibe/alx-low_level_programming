17475f6f32e3925d9d82453c5bd6ec4690d22013
George-Kibe <georgekibewambui@gmail.com>
2023-04-10 23:26:20 +0300

    Bit manipulation all

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of an unsigned long int
+ * @n: the number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	int i;
+
+	mask <<= (sizeof(unsigned long int) * 8) - 1;
+
+	for (i = 0; i < (int)(sizeof(unsigned long int) * 8); i++)
+	{
+		if ((n & mask) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+
+		mask >>= 1;
+	}
+}
