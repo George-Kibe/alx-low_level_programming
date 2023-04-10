d568dcea56293ea6fd16c391246b709a986d0775
George-Kibe <georgekibewambui@gmail.com>
2023-04-10 23:38:17 +0300

    correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_binary - prints the binary representation of an unsigned long int
@@ -6,18 +7,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-	int i;
-
-	mask <<= (sizeof(unsigned long int) * 8) - 1;
-
-	for (i = 0; i < (int)(sizeof(unsigned long int) * 8); i++)
-	{
-		if ((n & mask) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-
-		mask >>= 1;
-	}
+    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+    int started = 0; 
+    
+    while (mask > 0) {
+        if (n & mask) {
+            putchar('1'); 
+            started = 1; 
+        } else if (started) {
+            putchar('0'); 
+        }
+        
+        mask >>= 1; 
+    }
+    
+    if (!started) {
+        putchar('0'); 
+    }
 }
